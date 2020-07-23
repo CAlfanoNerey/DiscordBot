a47dd7986fa0a064bc0b71a1598a7eb6b18f27ed
Christian <CAlfano1999@gmail.com>
2020-07-23 18:00:41 -0400

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -1,7 +1,7 @@
 import discord
 import os
 from discord.ext import commands
-
+import random
 client = commands.Bot(command_prefix = ".")
 
 @client.event
@@ -16,4 +16,17 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'{member} has been sent to the gulag!')
 
+@client.command(aliases = ['magicconch','conch'])
+async def _mconch(ctx, *,question):
+    responses = ["Yes","No","Maybe"]
+    await ctx.send(f'Question: {question} \n Answer: {random.choice(responses)}')
+
+@client.command()
+async def clear(ctx, amount = 3):
+    await ctx.channel.purge(limit = amount)
+
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason = None):
+    await member.kick(reason = reason)
+    
 client.run(os.environ.get('MendBot_token'))
